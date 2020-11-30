2cff3b2743540765546aabe32b028a708d51821e
Kenneth Jones <kennethjones1991@outlook.com>
2020-11-30 13:01:51 -0700

    Created and committed initial project

---
+++ TimeTrack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeTrack
+//
+//  Created by Kenneth Jones on 11/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
